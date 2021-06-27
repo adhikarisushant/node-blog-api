0f1906cf9284a6c464a2e929aa4ce1399406acbc
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-06-27 16:04:19 +0545

    req query, GET and POST category, upload file rest api

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,13 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const CategorySchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    }
-}, {timestamps: true}
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", CategorySchema)
+module.exports = mongoose.model("Category", CategorySchema);
--- models/User.js
+++ models/User.js
@@ -1,25 +1,27 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    }, 
-    password:{
-        type:String,
-        required: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    profilePic:{
-        type: String,
-        default: "",
+    password: {
+      type: String,
+      required: true,
     },
-}, {timestamps: true}
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model("User", UserSchema);
